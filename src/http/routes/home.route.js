b3e3945f1e627e478efbef02471ee863ec1c2020
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-07-30 11:56:58 -0300

    add example for post route

--- src/http/routes/home.route.js
+++ src/http/routes/home.route.js
@@ -3,6 +3,13 @@ const home = (server) => {
     version: '0.1.0',
     message: 'Enjoy the silence...',
   }))
+
+  server.post('/', (req, reply) => {
+    reply.send({
+      ...req.body,
+      message: 'post url',
+    })
+  })
 }
 
 module.exports = home
