2c72e40fbc2ff3767da464cb198b4a1ec754f1af
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-08-03 14:51:12 -0300

    add integration for create todo

--- src/server/todos/index.js
+++ src/server/todos/index.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-restricted-syntax */
 const { badRequest, missingParamsError, ok } = require('../../helpers')
-const { getAllTodos } = require('../../services/todo')
+const { getAllTodos, createTodo } = require('../../services/todo')
 
 const add = (todo) => {
   const requiredFields = ['title', 'description']
 
   for (const field of requiredFields) {
-    if (!todo[field]) {
+    if (todo.completed !== null && !todo[field]) {
       return badRequest(missingParamsError(field))
     }
   }
+
+  createTodo(todo)
 
   return ok('success')
 }
--- src/services/todo/index.js
+++ src/services/todo/index.js
@@ -1,12 +1,20 @@
-const knex = require('knex')
-const knexfile = require('../../../knexfile')
+const { uid } = require('uid')
 
-const db = knex(knexfile[process.env.NODE_ENV])
+const knex = require('../../../knexfile')
 
-const getAllTodos = db('todos')
+const getAllTodos = knex('todos')
   .select('id', 'title', 'description', 'completed')
   .then((todos) => todos)
 
+const createTodo = async ({ title, description, completed }) =>
+  knex('todos').insert({
+    id: uid(),
+    title,
+    description,
+    completed,
+  })
+
 module.exports = {
   getAllTodos,
+  createTodo,
 }
